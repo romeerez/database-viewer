1c2dcad7fb1a82e24c7328d1294f51e5a3e6a250
romeerez <romadzao@gmail.com>
2021-08-31 21:49:48 -0400

    Simplify query running in editor

--- frontend/src/components/Editor/executeWidget.ts
+++ frontend/src/components/Editor/executeWidget.ts
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { MutableRefObject, useState } from 'react';
 import * as monaco from 'monaco-editor';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { parseSql } from 'sql-autocomplete';
+import { editor } from 'monaco-editor';
+import ICodeEditor = editor.ICodeEditor;
 
 const widgetId = 'executeQuery.widget';
 
@@ -37,184 +39,203 @@ export const useExecuteWidget = ({
       contextMenuGroupId: 'navigation',
       contextMenuOrder: 1.5,
       run(editor) {
-        const position = editor.getPosition();
-        if (!position) return;
-
-        const model = editor.getModel();
-        if (!model) return;
-
-        const currentLine = position.lineNumber;
-        const currentColumn = position.column;
-
-        const fullRange = model.getFullModelRange();
-        let fullText = model.getValueInRange({
-          startLineNumber: 1,
-          startColumn: 0,
-          endLineNumber: fullRange.endLineNumber,
-          endColumn: fullRange.endColumn,
-        });
-
-        if (!fullText.match(/;\s*$/)) fullText += ';';
-
-        const parsedLocations = parseSql(fullText, '', false)
-          .locations as Location[];
-        const len = parsedLocations.length;
-
-        const locations: Location['location'][] = [];
-        let currentLocation: Location['location'] | undefined;
-
-        for (let i = 0; i < len; i++) {
-          const statement = parsedLocations[i];
-          if (statement.type === 'statement') {
-            currentLocation = statement.location;
-            if (
-              currentLocation.first_line > currentLine ||
-              (currentLocation.first_line === currentLine &&
-                currentLocation.first_column > currentColumn)
-            )
-              break;
-
-            locations.push(currentLocation);
-          } else if (currentLocation) {
-            currentLocation.last_line = statement.location.last_line;
-            currentLocation.last_column = statement.location.last_column;
-          }
+        const statements = getStatements(editor);
+        if (!statements || statements.length === 0) {
+          return;
         }
 
-        if (locations.length === 0 && parsedLocations.length) {
-          const first = parsedLocations[0].location;
-          const last = parsedLocations[len - 1].location;
-          locations.push({
-            first_line: first.first_line,
-            first_column: first.first_column,
-            last_line: last.last_line,
-            last_column: last.last_column,
-          });
+        if (statements.length === 1) {
+          if (executeQueryRef.current)
+            executeQueryRef.current(statements[0].query);
+          return;
         }
 
-        if (locations.length === 0) return;
-
-        const statements: Statement[] = [];
-        for (const location of locations) {
-          location.last_column++;
-
-          const statement: Statement = {
-            ...location,
-            query: model.getValueInRange({
-              startLineNumber: location.first_line,
-              startColumn: location.first_column,
-              endLineNumber: location.last_line,
-              endColumn: location.last_column,
-            }),
-          };
-
-          const match = statement.query.match(/^[;\s]+/);
-          if (match) {
-            const stripped = statement.query.slice(0, match[0].length);
-            const arr = stripped.split(/\n/);
-            statement.first_line += arr.length - 1;
-            statement.first_column = arr[arr.length - 1].length;
-
-            if (
-              statement.first_line > currentLine ||
-              (statement.first_line === currentLine &&
-                statement.first_column > currentColumn)
-            )
-              break;
-
-            statement.query = statement.query.slice(match[0].length);
-          }
-
-          statements.push(statement);
-        }
-
-        statements.reverse();
-
-        editor.addContentWidget({
-          getId() {
-            return widgetId;
-          },
-          getDomNode() {
-            const div = document.createElement('div');
-            widgetRef.current = div;
-            div.id = widgetId;
-            div.className =
-              'text-md bg-primary-gradient-lighter rounded py-1 shadow';
-
-            const close = () => {
-              document.removeEventListener('mousedown', listener);
-              div.remove();
-              widgetRef.current = null;
-              editor.focus();
-            };
-
-            const listener = (e: MouseEvent) => {
-              if (!(e.target as HTMLDivElement).closest(`#${widgetId}`))
-                close();
-            };
-            document.addEventListener('mousedown', listener);
-
-            div.onkeydown = (e) => {
-              if (e.key === 'Escape') {
-                close();
-              } else if (e.key === 'ArrowDown') {
-                const focused = div.querySelector(':focus');
-                if (focused?.nextElementSibling) {
-                  (focused.nextElementSibling as HTMLButtonElement).focus();
-                } else {
-                  (div.firstElementChild as HTMLButtonElement).focus();
-                }
-              } else if (e.key === 'ArrowUp') {
-                const focused = div.querySelector(':focus');
-                if (focused?.previousElementSibling) {
-                  (focused.previousElementSibling as HTMLButtonElement).focus();
-                } else {
-                  (div.lastElementChild as HTMLButtonElement).focus();
-                }
-              }
-            };
-
-            statements.forEach((statement) => {
-              const button = document.createElement('button');
-              const { query } = statement;
-
-              button.className =
-                'h-8 whitespace-nowrap px-3 text-light-3 hover:text-light-1 hover:bg-lighter';
-              button.textContent = query;
-              button.onfocus = () => {
-                editor.setSelection({
-                  startLineNumber: statement.first_line,
-                  startColumn: statement.first_column,
-                  endLineNumber: statement.last_line,
-                  endColumn: statement.last_column,
-                });
-              };
-              button.onclick = () => {
-                close();
-                if (executeQueryRef.current) executeQueryRef.current(query);
-              };
-              div.appendChild(button);
-            });
-
-            return div;
-          },
-          afterRender() {
-            const el = widgetRef.current as HTMLDivElement;
-            (el.firstElementChild as HTMLButtonElement).focus();
-          },
-          getPosition: function () {
-            return {
-              position: editor.getPosition(),
-              preference: [
-                monaco.editor.ContentWidgetPositionPreference.ABOVE,
-                monaco.editor.ContentWidgetPositionPreference.BELOW,
-              ],
-            };
-          },
-        });
+        addWidget(editor, widgetRef, statements, executeQueryRef);
       },
     });
   };
 
   return { addExecuteAction };
+};
+
+const getStatements = (editor: ICodeEditor) => {
+  const position = editor.getPosition();
+  if (!position) return;
+
+  const model = editor.getModel();
+  if (!model) return;
+
+  const currentLine = position.lineNumber;
+  const currentColumn = position.column;
+
+  const fullRange = model.getFullModelRange();
+  let fullText = model.getValueInRange({
+    startLineNumber: 1,
+    startColumn: 0,
+    endLineNumber: fullRange.endLineNumber,
+    endColumn: fullRange.endColumn,
+  });
+
+  if (!fullText.match(/;\s*$/)) fullText += ';';
+
+  const parsedLocations = parseSql(fullText, '', false).locations as Location[];
+  const len = parsedLocations.length;
+
+  const locations: Location['location'][] = [];
+  let currentLocation: Location['location'] | undefined;
+
+  for (let i = 0; i < len; i++) {
+    const statement = parsedLocations[i];
+    if (statement.type === 'statement') {
+      currentLocation = statement.location;
+      if (
+        currentLocation.first_line > currentLine ||
+        (currentLocation.first_line === currentLine &&
+          currentLocation.first_column > currentColumn)
+      )
+        break;
+
+      locations.push(currentLocation);
+    } else if (currentLocation) {
+      currentLocation.last_line = statement.location.last_line;
+      currentLocation.last_column = statement.location.last_column;
+    }
+  }
+
+  if (locations.length === 0 && parsedLocations.length) {
+    const first = parsedLocations[0].location;
+    const last = parsedLocations[len - 1].location;
+    locations.push({
+      first_line: first.first_line,
+      first_column: first.first_column,
+      last_line: last.last_line,
+      last_column: last.last_column,
+    });
+  }
+
+  if (locations.length === 0) return;
+
+  const statements: Statement[] = [];
+  for (const location of locations) {
+    location.last_column++;
+
+    const statement: Statement = {
+      ...location,
+      query: model.getValueInRange({
+        startLineNumber: location.first_line,
+        startColumn: location.first_column,
+        endLineNumber: location.last_line,
+        endColumn: location.last_column,
+      }),
+    };
+
+    const match = statement.query.match(/^[;\s]+/);
+    if (match) {
+      const stripped = statement.query.slice(0, match[0].length);
+      const arr = stripped.split(/\n/);
+      statement.first_line += arr.length - 1;
+      statement.first_column = arr[arr.length - 1].length;
+
+      if (
+        statement.first_line > currentLine ||
+        (statement.first_line === currentLine &&
+          statement.first_column > currentColumn)
+      )
+        break;
+
+      statement.query = statement.query.slice(match[0].length);
+    }
+
+    statements.push(statement);
+  }
+  statements.reverse();
+  return statements;
+};
+
+const addWidget = (
+  editor: ICodeEditor,
+  widgetRef: MutableRefObject<HTMLDivElement | null>,
+  statements: Statement[],
+  executeQueryRef: MutableRefObject<((query: string) => void) | undefined>,
+) => {
+  editor.addContentWidget({
+    getId() {
+      return widgetId;
+    },
+    getDomNode() {
+      const div = document.createElement('div');
+      widgetRef.current = div;
+      div.id = widgetId;
+      div.className = 'text-md bg-primary-gradient-lighter rounded py-1 shadow';
+
+      const close = () => {
+        document.removeEventListener('mousedown', listener);
+        div.remove();
+        widgetRef.current = null;
+        editor.focus();
+      };
+
+      const listener = (e: MouseEvent) => {
+        if (!(e.target as HTMLDivElement).closest(`#${widgetId}`)) close();
+      };
+      document.addEventListener('mousedown', listener);
+
+      div.onkeydown = (e) => {
+        if (e.key === 'Escape') {
+          close();
+        } else if (e.key === 'ArrowDown') {
+          const focused = div.querySelector(':focus');
+          if (focused?.nextElementSibling) {
+            (focused.nextElementSibling as HTMLButtonElement).focus();
+          } else {
+            (div.firstElementChild as HTMLButtonElement).focus();
+          }
+        } else if (e.key === 'ArrowUp') {
+          const focused = div.querySelector(':focus');
+          if (focused?.previousElementSibling) {
+            (focused.previousElementSibling as HTMLButtonElement).focus();
+          } else {
+            (div.lastElementChild as HTMLButtonElement).focus();
+          }
+        }
+      };
+
+      statements.forEach((statement) => {
+        const button = document.createElement('button');
+        const { query } = statement;
+
+        button.className =
+          'h-8 whitespace-nowrap px-3 text-light-3 hover:text-light-1 hover:bg-lighter';
+        button.textContent = query;
+        button.onfocus = () => {
+          editor.setSelection({
+            startLineNumber: statement.first_line,
+            startColumn: statement.first_column,
+            endLineNumber: statement.last_line,
+            endColumn: statement.last_column,
+          });
+        };
+        button.onclick = () => {
+          close();
+          if (executeQueryRef.current) executeQueryRef.current(query);
+        };
+        div.appendChild(button);
+      });
+
+      return div;
+    },
+    afterRender() {
+      const el = widgetRef.current as HTMLDivElement;
+      (el.firstElementChild as HTMLButtonElement).focus();
+    },
+    getPosition: function () {
+      return {
+        position: editor.getPosition(),
+        preference: [
+          monaco.editor.ContentWidgetPositionPreference.ABOVE,
+          monaco.editor.ContentWidgetPositionPreference.BELOW,
+        ],
+      };
+    },
+  });
 };
